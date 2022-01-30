acafa2254691bb8571ea5e444b5814dfb643a366
Sergey Kuklin <ksv.crowley@gmail.com>
2022-01-30 23:33:57 +0500

    refactor: Extracts default config

--- src/screens/JsonToFormScreen/JsonToFormScreen.tsx
+++ src/screens/JsonToFormScreen/JsonToFormScreen.tsx
@@ -1,67 +1,14 @@
 import { useCallback, useMemo, useState } from 'react';
 
+import { FormConfig } from 'features/FormFromJson/types';
+
 import { FormFromJson } from 'features/FormFromJson';
 import { PrettyJsonTextarea } from 'features/PrettyJsonTextarea';
-import { FormConfig } from 'features/FormFromJson/types';
 
 import { Tabs } from 'shared/components/Tabs';
 import { DefaultLayout } from 'shared/layouts/DefaultLayout';
 
-const defaultJsonConfig: FormConfig = {
-  title: 'Form',
-  fields: [
-    {
-      label: 'Text',
-      id: 'text',
-      name: 'text',
-      type: 'text'
-    },
-    {
-      label: 'Textarea',
-      id: 'textarea',
-      name: 'textarea',
-      type: 'textarea'
-    },
-    {
-      label: 'Date',
-      id: 'date',
-      name: 'date',
-      type: 'date',
-      value: '2020-01-01'
-    },
-    {
-      label: 'Number',
-      id: 'number',
-      name: 'number',
-      type: 'number',
-      value: 3
-    },
-    {
-      label: 'Checkbox',
-      id: 'checkbox',
-      name: 'checkbox',
-      type: 'checkbox',
-      value: false
-    },
-    {
-      label: 'Radio',
-      name: 'radio',
-      id: 'radio',
-      type: 'radio',
-      value: 'radio-1',
-      options: [
-        {
-          label: 'Radio 1',
-          value: 'radio-1'
-        },
-        {
-          label: 'Radio 2',
-          value: 'radio-2'
-        }
-      ]
-    }
-  ]
-};
+import { defaultJsonConfig } from './defaultJsonConfig';
 
 export const JsonToFormScreen = () => {
   const [json, setJson] = useState<FormConfig>(defaultJsonConfig);
